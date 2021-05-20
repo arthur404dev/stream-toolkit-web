2a3b54f68e54fea4c060ab16c3daca861f2ba62e
Arthur Andrade <arthur.diegoo@hotmail.com>
2021-05-20 01:08:18 -0300

    feat: add redirects for deployment and cleanup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import useMessages from './hooks/useMessages'
 import Routes from './Routes'
 
 function App() {
   const [messages, scroll] = useMessages()
-  return <Routes messageState={[messages, scroll]} />
+  return (
+    <Router>
+      <Routes messageState={[messages, scroll]} />
+      {/* <Link to={`/overlay/chat-vertical`}>Chat-Vertical</Link> */}
+    </Router>
+  )
 }
 
 export default App
