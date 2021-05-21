443198584843123ed1dea9078e39faa946b86e43
Arthur Andrade <arthur.diegoo@hotmail.com>
2021-05-21 20:50:26 -0300

    feat: divided page-size by view

--- src/pages/ChatVertical.jsx
+++ src/pages/ChatVertical.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Message from '../components/Message'
+import './ChatVertical.css'
 
 const Messages = ({ state }) => {
   const [messages, scroll] = state
   return (
-    <main className='bg-transparent w-full h-full flex-col'>
-      {messages.map((message) => (
-        <Message key={message.id} payload={message} />
-      ))}
-      <div ref={scroll} />
-    </main>
+    <div className='page'>
+      <main className='bg-transparent w-full h-full flex-col'>
+        {messages.map((message) => (
+          <Message key={message.id} payload={message} />
+        ))}
+        <div ref={scroll} />
+      </main>
+    </div>
   )
 }
 
